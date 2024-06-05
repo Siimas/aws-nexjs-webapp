c3f51a03f6869f7f37cec4f80b1b661239d2bc0a
Siimas <simaomarques20@gmail.com>
2024-06-05 15:30:07 +0100

    Update CICD pipelines

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,16 +1,11 @@
 
 
 export default function Home() {
-
   const currentEnvironment = process.env.ENVIRONMENT
-
-  const test = process.env.ENVIRONMENT === "PRODUCTION" ? <div>PROD</div> : <div>DEV</div>
 
   return (
     <main className="flex justify-center items-center">
       <p>Current Environment: {currentEnvironment}</p>
-      <br />
-      {test}
     </main>
   );
 }
